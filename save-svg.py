4d57fa61543cd16c14568a70f8377f1872b35371
QuanNH <quannh@ominext.com>
2020-02-28 08:42:57 +0700

    rename

--- save-svg.py
+++ save-svg.py
@@ -3,3 +3,17 @@ https://upload.wikimedia.org/wikipedia/commons/5/5b/Vietnam_road_sign_P101.svg
 https://upload.wikimedia.org/wikipedia/commons/f/f3/Vietnam_road_sign_P102.svg
 https://upload.wikimedia.org/wikipedia/commons/9/9c/Vietnam_road_sign_P103a.svg
 '''
+import os
+
+
+def rename():
+  sign_dir = f'road-sign'
+  for root, _, files in os.walk(sign_dir):
+    for f in files:
+      if 'Vietnam_road_sign_' in f:
+        f_new = f.replace('Vietnam_road_sign_','').lower()
+        os.rename( f'{root}/{f}',f'{root}/{f_new}')
+
+
+if __name__ == "__main__":
+    rename()
